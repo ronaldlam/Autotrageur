84c87f54560fe2a521889b3b17473ec58a36328f
Ronald Lam <ronaldctlam@gmail.com>
2018-03-23 02:26:41 -0700

    Code review changes

--- bot/orderbook_processor.py
+++ bot/orderbook_processor.py
@@ -1,4 +1,4 @@
-def get_price_from_orderbook(bids_or_asks, amount):
+def get_price_from_orderbook(bids_or_asks, target_amount):
     """Get market buy or sell price
 
     Return potential market buy or sell price given bids or asks and
@@ -6,9 +6,9 @@ def get_price_from_orderbook(bids_or_asks, amount):
     input of asks will retrieve market buy price.
 
     Args:
-        bids_or_asks ((int, int)): The bids or asks in the form of
+        bids_or_asks (list[(int, int)]): The bids or asks in the form of
             (price, volume).
-        amount (int): The amount to prospectively market buy.
+        target_amount (int): The amount to prospectively market buy.
 
     Returns:
         float: Prospective price of a market sell.
@@ -19,19 +19,19 @@ def get_price_from_orderbook(bids_or_asks, amount):
 
     index = 0
     asset_amount = 0.0
-    original_amount = amount
+    original_amount = target_amount
 
     # Subtract from amount until enough of the order book is eaten up by
     # the trade.
-    while amount > 0.0 and index < len(bids_or_asks):
-        amount -= bids_or_asks[index][0] * bids_or_asks[index][1]
+    while target_amount > 0.0 and index < len(bids_or_asks):
+        target_amount -= bids_or_asks[index][0] * bids_or_asks[index][1]
         asset_amount += bids_or_asks[index][1]
         index += 1
 
-    if index == len(bids_or_asks) and amount > 0.0:
+    if index == len(bids_or_asks) and target_amount > 0.0:
         raise RuntimeError("Order book not deep enough for trade.")
 
     # Add the zero or negative excess amount to trim off the overshoot
-    asset_amount += amount / bids_or_asks[index - 1][0]
+    asset_amount += target_amount / bids_or_asks[index - 1][0]
 
     return original_amount/asset_amount
