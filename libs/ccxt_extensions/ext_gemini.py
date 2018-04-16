3460913bb2d1a257bbb9d515b99849adcb669cdb
Ronald Lam <ronaldctlam@gmail.com>
2018-04-16 15:32:14 -0700

    Add trade limits data for ext_gemini.py

--- libs/ccxt_extensions/ext_gemini.py
+++ libs/ccxt_extensions/ext_gemini.py
@@ -38,9 +38,9 @@ class ext_gemini(ccxt.gemini):
         """Retrieve data for the markets of the exchange.
 
         This gets called by load_markets() which dynamically fetches and
-        populates information about a given exchange. Precision data is
-        added here for consistency. The ccxt.binance() module was used
-        for reference.
+        populates information about a given exchange. Precision and
+        limit data is added here for consistency. The ccxt.binance()
+        module was used for reference.
 
         Returns:
             dict: The description of available markets on the exchange.
@@ -66,11 +66,44 @@ class ext_gemini(ccxt.gemini):
                 'price': 5,
             }
         }
+        limits = {
+            'BTC/USD': {
+                'amount': {
+                    'min': 0.00001,     # Only min order amounts are specified
+                    'max': None,
+                },
+                'price': {
+                    'min': None,
+                    'max': None,
+                }
+            },
+            'ETH/USD': {
+                'amount': {
+                    'min': 0.001,
+                    'max': None,
+                },
+                'price': {
+                    'min': None,
+                    'max': None,
+                }
+            },
+            'ETH/BTC': {
+                'amount': {
+                    'min': 0.001,
+                    'max': None,
+                },
+                'price': {
+                    'min': None,
+                    'max': None,
+                }
+            }
+        }
 
         markets = super().fetch_markets()
 
         for market in markets:
             market['precision'] = precision[market['symbol']]
+            market['limits'] = limits[market['symbol']]
 
         return markets
 
