cc1db904f0dc87b813e4c37d888c76c4f552add0
Drew Wei <drewngui@gmail.com>
2018-04-28 16:38:54 -0700

    WIP: Fetcher tests and refactor

--- tests/unit/libs/fetcher/test_ccxt_fetcher.py
+++ tests/unit/libs/fetcher/test_ccxt_fetcher.py
@@ -1,66 +1,140 @@
 import ccxt
 import pytest
-# from pytest_mock import mocker
+from pytest_mock import mocker
 
-from tests.unit.resources.mock_exchanges import (
-    fake_binance,
-    fake_generic_exchange)
+from tests.unit.resources.mock_exchanges import fake_binance
 from libs.trade.fetcher.ccxt_fetcher import CCXTFetcher
 
-bad_fee_structures = [
-    {},
-    {
-        'trading': {
-            'maker': 0.25
-        }
-    },
-    {
-        'trading': {
-            'maker': 0.25,
-            'taker': 0.10
-        }
-    },
-    {
+
+xfail = pytest.mark.xfail
+
+
+# Test constants.
+MAKER_FEE = 0.25
+TAKER_FEE = 0.10
+BTC_FREE_BALANCE = 1.50
+USD_FREE_BALANCE = 123.00
+
+
+fee_structures = {
+    'none': None,
+    'empty': {},
+    'no_trading':  {
         'garbage': {
-            'maker': 0.25,
-            'taker': 0.10
+            'maker': MAKER_FEE,
+            'taker': TAKER_FEE
+        }
+    },
+    'maker_only': {
+        'trading': {
+            'maker': MAKER_FEE
+        }
+    },
+    'taker_only': {
+        'trading': {
+            'taker': TAKER_FEE
+        }
+    },
+    'full': {
+        'trading': {
+            'maker': MAKER_FEE,
+            'taker': TAKER_FEE
         }
     }
-]
+}
 
+balance_structures = {
+    'none': None,
+    'empty': {},
+    'no_free': {
+        'BTC': {
+            'used': 0.00,
+            'total': 1.50,
+        },
+        'USD': {
+            'used': 456.00,
+            'total': 579.00,
+        }
+    },
+    'full': {
+        'BTC': {
+            'free': BTC_FREE_BALANCE,
+            'used': 0.00,
+            'total': 1.50,
+        },
+        'USD': {
+            'free': USD_FREE_BALANCE,
+            'used': 456.00,
+            'total': 579.00,
+        }
+    }
+
+}
 
 @pytest.fixture()
 def ccxtfetcher_binance(fake_binance):
     return CCXTFetcher(fake_binance)
 
-@pytest.fixture(params=bad_fee_structures)
-def ccxtfetcher_binance_bad_fees(fake_binance, request):
-    fake_binance.fees =
-    return CCXTFetcher(fake_binance)
+# @pytest.fixture(params=fee_structures)
+# def mock_fee_structure
 
 @pytest.mark.parametrize('exchange', [
     ccxt.binance(),
-    pytest.param("binance", marks=pytest.mark.xfail(raises=TypeError, reason="string not ccxt object", strict=True)),
-    pytest.param("", marks=pytest.mark.xfail(raises=TypeError, reason="empty string not ccxt object", strict=True)),
-    pytest.param(None, marks=pytest.mark.xfail(raises=TypeError, reason="None not ccxt object", strict=True)),
+    pytest.param("binance", marks=xfail(raises=TypeError, reason="string not ccxt object", strict=True)),
+    pytest.param("", marks=xfail(raises=TypeError, reason="empty string not ccxt object", strict=True)),
+    pytest.param(None, marks=xfail(raises=TypeError, reason="None not ccxt object", strict=True)),
 ])
 def test_init(exchange):
     fetcher = CCXTFetcher(exchange)
     assert fetcher.exchange == exchange
 
 
-@pytest.mark.parametrize('ccxt_fetcher', [
-    ccxtfetcher_binance
+@pytest.mark.parametrize('fee_structure_key, maker_fee', [
+    pytest.param('none', MAKER_FEE, marks=xfail(raises=TypeError, reason="NoneType object is not subscriptable", strict=True)),
+    pytest.param('empty', MAKER_FEE, marks=xfail(raises=KeyError, reason="No keys in empty dict", strict=True)),
+    pytest.param('no_trading', MAKER_FEE, marks=xfail(raises=KeyError, reason="No trading key", strict=True)),
+    pytest.param('taker_only', MAKER_FEE, marks=xfail(raises=KeyError, reason="No maker key", strict=True)),
+    ('maker_only', MAKER_FEE),
+    ('full', MAKER_FEE)
 ])
-def test_fetch_maker_fees(ccxt_fetcher):
-    maker_fees = ccxt_fetcher.fetch_maker_fees()
-    assert maker_fees == 0.25
+def test_fetch_maker_fees(mocker, ccxtfetcher_binance, fee_structure_key, maker_fee):
+    mocker.patch.object(ccxtfetcher_binance.exchange, 'fees', fee_structures[fee_structure_key])
+    maker_fees = ccxtfetcher_binance.fetch_maker_fees()
+    assert type(maker_fees) is float
+    assert maker_fees == MAKER_FEE
 
-# def test_fetch_maker_fees_full(ccxtfetcher_binance):
-#     maker_fees = ccxtfetcher_binance.fetch_maker_fees()
-#     assert maker_fees == 0.25
 
-# def test_fetch_maker_fees_feeless(mocker, ccxtfetcher_binance):
-#     mocker.patch.object(ccxtfetcher_binance, 'fees', {})
-#     maker_fees = ccxtfetcher_binance.fetch_maker_fees()
-#     assert maker_fees == 0.25
+@pytest.mark.parametrize('fee_structure_key, taker_fee', [
+    pytest.param('none', TAKER_FEE, marks=xfail(raises=TypeError, reason="NoneType object is not subscriptable", strict=True)),
+    pytest.param('empty', TAKER_FEE, marks=xfail(raises=KeyError, reason="No keys in empty dict", strict=True)),
+    pytest.param('no_trading', TAKER_FEE, marks=xfail(raises=KeyError, reason="No trading key", strict=True)),
+    pytest.param('maker_only', TAKER_FEE, marks=xfail(raises=KeyError, reason="No taker key", strict=True)),
+    ('taker_only', TAKER_FEE),
+    ('full', TAKER_FEE)
+])
+def test_fetch_taker_fees(mocker, ccxtfetcher_binance, fee_structure_key, taker_fee):
+    mocker.patch.object(ccxtfetcher_binance.exchange, 'fees', fee_structures[fee_structure_key])
+    taker_fees = ccxtfetcher_binance.fetch_taker_fees()
+    assert type(taker_fees) is float
+    assert taker_fees == TAKER_FEE
+
+
+@pytest.mark.parametrize('asset, balance_structure_key, asset_free_balance', [
+    pytest.param('BTC', 'none', BTC_FREE_BALANCE, marks=xfail(raises=TypeError, reason="NoneType object is not subscriptable", strict=True)),
+    pytest.param('BTC', 'empty', BTC_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No keys in empty dict", strict=True)),
+    pytest.param('BTC', 'no_free', BTC_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No free key", strict=True)),
+    ('BTC', 'full', BTC_FREE_BALANCE),
+    ('USD', 'full', USD_FREE_BALANCE)
+])
+def test_fetch_free_balance(mocker, asset, balance_structure_key, asset_free_balance, ccxtfetcher_binance):
+    mocker.patch.object(ccxtfetcher_binance.exchange, 'fetch_balance', return_value=balance_structures[balance_structure_key])
+    free_balance = ccxtfetcher_binance.fetch_free_balance(asset)
+    assert type(free_balance) is float
+    assert free_balance == asset_free_balance
+
+
+def test_get_full_orderbook(mocker, ccxtfetcher_binance):
+    mocker.patch.object(ccxtfetcher_binance.exchange, 'fetch_order_book')
+    ccxtfetcher_binance.get_full_orderbook('BTC', 'USD')
+    ccxtfetcher_binance.exchange.fetch_order_book.assert_called_once_with('BTC/USD')
+# return self.exchange.fetch_order_book(base + "/" + quote)
--- tests/unit/resources/mock_exchanges.py
+++ tests/unit/resources/mock_exchanges.py
@@ -1,22 +1,30 @@
 import ccxt
 import pytest
 
-@pytest.fixture()
-def fake_generic_exchange(mocker):
-    fake_exchange = mocker.patch.object(ccxt, 'Exchange', autospec=True)
-    fake_exchange.fees = {
-        'trading': {
-            'maker': 0.25
-        }
-    }
-    return fake_exchange
 
 @pytest.fixture()
 def fake_binance(mocker):
     fake_binance_exchange = mocker.patch.object(ccxt, 'binance', autospec=True)
+
+    # Fake exchange fees.
     fake_binance_exchange.fees = {
         'trading': {
-            'maker': 0.25
+            'maker': 0.25,
+            'taker': 0.10
+        }
+    }
+
+    # Fake account balances.
+    fake_binance_exchange.balances = {
+        'BTC': {
+            'free': 1.50,
+            'used': 0.00,
+            'total': 1.50,
+        },
+        'USD': {
+            'free': 123.00,
+            'used': 456.00,
+            'total': 579.00,
         }
     }
     return fake_binance_exchange
