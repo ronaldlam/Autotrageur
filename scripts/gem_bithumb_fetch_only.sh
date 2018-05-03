2f6504262c3f1f2e40b8f2c035bdc03b97dd7324
Drew Wei <drewngui@gmail.com>
2018-05-03 02:23:05 -0700

    sample dryrun and sample scripts + Readme

---
+++ scripts/gem_bithumb_fetch_only.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set the venv.
+if source venv/Scripts/activate; then
+    echo "Activated venv windows"
+else
+    source venv/bin/activate
+    echo "Activated venv unix"
+fi
+
+# Run the main module.
+python run_autotrageur.py <SECRET_KEY_FILE> <PASSPHRASE> <SALT> configs/dry_gem_bithumb_eth.yaml
