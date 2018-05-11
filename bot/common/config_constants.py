45f786a3af81cab8195e816cd71d652ff43f7445
Drew Wei <drewngui@gmail.com>
2018-05-10 23:24:33 -0700

    Refactor constants out into common/

---
+++ bot/common/config_constants.py
@@ -0,0 +1,20 @@
+"""Configuration file constants."""
+
+# Email constants.
+EMAIL_CFG_PATH = 'email_cfg_path'
+MAX_EMAILS = 'max_emails'
+SPREAD_ROUNDING = 'spread_rounding'
+SPREAD_TOLERANCE = 'spread_tolerance'
+
+# Trade configuration constants.
+DRYRUN = "dryrun"
+SLIPPAGE = "slippage"
+EXCHANGE1 = "exchange1"
+EXCHANGE2 = "exchange2"
+EXCHANGE1_PAIR = "exchange1_pair"
+EXCHANGE2_PAIR = "exchange2_pair"
+EXCHANGE1_TEST = "exchange1_test"
+EXCHANGE2_TEST = "exchange2_test"
+SPREAD_TARGET_LOW = "spread_target_low"
+SPREAD_TARGET_HIGH = "spread_target_high"
+TARGET_AMOUNT = "target_amount"
