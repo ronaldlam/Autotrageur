0c9de21eab747235c12efeacf2d0626ccd7e6f0b
jaonewguy <drewngui@gmail.com>
2018-06-17 00:49:30 -0700

    Autotrageur, arbseeker and ccxt_fetcher tests

--- tests/unit/bot/arbitrage/test_autotrageur.py
+++ tests/unit/bot/arbitrage/test_autotrageur.py
@@ -1,3 +1,5 @@
+import getpass
+
 import ccxt
 import pytest
 import schedule
@@ -55,6 +57,7 @@ def test_load_config_file(mocker, autotrageur):
 )
 def test_load_keyfile(mocker, autotrageur, succeed, dryrun):
     args = mocker.MagicMock()
+    mocker.patch('getpass.getpass')
     mocker.patch.dict(autotrageur.config, { DRYRUN: dryrun })
 
     if succeed:
@@ -119,7 +122,6 @@ def test_setup_markets(
         mocker, autotrageur, ex1_test, ex2_test, client_quote_usd,
         balance_check, dryrun):
     fake_slippage = 0.25
-    fake_target_amount = 12345
     fake_pair = 'fake/pair'
     placeholder = 'fake'
     trader = mocker.patch('bot.arbitrage.autotrageur.CCXTTrader')
@@ -143,7 +145,7 @@ def test_setup_markets(
         # Set a fiat quote pair that is not USD to trigger conversion calls.
         instance.quote = 'KRW'
     if not balance_check:
-        instance.check_wallet_balances.side_effect = ccxt.AuthenticationError()
+        instance.fetch_wallet_balances.side_effect = ccxt.AuthenticationError()
 
     mocker.patch.dict(autotrageur.config, configuration)
     mocker.spy(schedule, 'every')
@@ -163,16 +165,16 @@ def test_setup_markets(
         # `conversion_needed` not set in the patched trader instance.
         assert(schedule.every.call_count == 0)          # pylint: disable=E1101
         assert len(schedule.jobs) == 0
-        assert(instance.set_conversion.call_count == 0)
+        assert(instance.set_forex_ratio.call_count == 0)
     else:
         assert instance.conversion_needed is True
         assert(schedule.every.call_count == 2)          # pylint: disable=E1101
         assert len(schedule.jobs) == 2
-        assert(instance.set_conversion.call_count == 2)
-
-    if balance_check:
-        assert(instance.check_wallet_balances.call_count == 2)
-    else:
-        assert(dryrun)
+        assert(instance.set_forex_ratio.call_count == 2)
 
     schedule.clear()
+
+    if balance_check:
+        assert(instance.fetch_wallet_balances.call_count == 2)
+    else:
+        assert(dryrun)
--- tests/unit/libs/trade/fetcher/test_ccxt_fetcher.py
+++ tests/unit/libs/trade/fetcher/test_ccxt_fetcher.py
@@ -12,8 +12,8 @@ xfail = pytest.mark.xfail
 # Test constants.
 MAKER_FEE = Decimal('0.25')
 TAKER_FEE = Decimal('0.10')
-BTC_FREE_BALANCE = 1.50
-USD_FREE_BALANCE = 123.00
+BTC_FREE_BALANCE = Decimal('1.50')
+USD_FREE_BALANCE = Decimal('123.00')
 BTC_LAST = 120.00
 
 
@@ -131,19 +131,21 @@ def test_fetch_taker_fees(mocker, ccxtfetcher_binance, fee_structure_key, taker_
     assert taker_fees == TAKER_FEE
 
 
-@pytest.mark.parametrize('asset, balance_structure_key, asset_free_balance', [
-    pytest.param('BTC', 'none', BTC_FREE_BALANCE, marks=xfail(raises=TypeError, reason="NoneType object is not subscriptable", strict=True)),
-    pytest.param('BTC', 'empty', BTC_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No keys in empty dict", strict=True)),
-    pytest.param('BTC', 'no_free', BTC_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No free key", strict=True)),
-    ('BTC', 'full', BTC_FREE_BALANCE),
-    ('USD', 'full', USD_FREE_BALANCE)
+@pytest.mark.parametrize('base, quote, balance_structure_key, base_free_balance, quote_free_balance', [
+    pytest.param('BTC', 'USD', 'none', BTC_FREE_BALANCE, USD_FREE_BALANCE, marks=xfail(raises=TypeError, reason="NoneType object is not subscriptable", strict=True)),
+    pytest.param('BTC', 'USD', 'empty', BTC_FREE_BALANCE, USD_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No keys in empty dict", strict=True)),
+    pytest.param('BTC', 'USD', 'no_free', BTC_FREE_BALANCE, USD_FREE_BALANCE, marks=xfail(raises=KeyError, reason="No free key", strict=True)),
+    ('BTC', 'USD', 'full', BTC_FREE_BALANCE, USD_FREE_BALANCE)
+    # ('BTC', 'USD', 'full', BTC_FREE_BALANCE, USD_FREE_BALANCE)
 ])
-def test_fetch_free_balance(mocker, asset, balance_structure_key, asset_free_balance, ccxtfetcher_binance):
+def test_fetch_free_balances(mocker, base, quote, balance_structure_key,
+                             base_free_balance, quote_free_balance, ccxtfetcher_binance):
     mocker.patch.object(ccxtfetcher_binance.exchange, 'fetch_balance', return_value=balance_structures[balance_structure_key])
-    free_balance = ccxtfetcher_binance.fetch_free_balance(asset)
+    (btc_balance, usd_balance) = ccxtfetcher_binance.fetch_free_balances(base, quote)
     ccxtfetcher_binance.exchange.fetch_balance.assert_called_once_with()
-    assert type(free_balance) is float
-    assert free_balance == asset_free_balance
+    assert type(btc_balance) is Decimal
+    assert type(usd_balance) is Decimal
+    assert (btc_balance, usd_balance) == (base_free_balance, quote_free_balance)
 
 
 @pytest.mark.parametrize('base, quote', [
