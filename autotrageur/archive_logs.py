e5d870215431df87084f43d7c35887f191e941f0
Ronald Lam <ronaldctlam@gmail.com>
2018-11-01 01:44:18 -0700

    Correct dir lookup, name archived files.

--- autotrageur/archive_logs.py
+++ autotrageur/archive_logs.py
@@ -18,6 +18,9 @@ from docopt import docopt
 from autotrageur.version import VERSION
 
 
+LOGS = 'logs'
+
+
 def archive_logs():
     """Compresses older log files into zip file in the same directory.
 
@@ -26,43 +29,33 @@ def archive_logs():
 
     Each directory in the 'logs' directory will be searched for
     concurrent operating bot support. Archives are only created if there
-    logs to archive. Old files are deleted after archiving.
+    logs to archive. Old files are deleted after archiving. The archives
+    are created in zip files named by the timestamp at the time of
+    archive, in the location of the existing files.
     """
     logging.info('Archive start...')
 
-    log_types = [d for d in os.listdir('logs') if isdir(join('logs', d))]
+    log_types = [join(LOGS, d) for d in os.listdir(LOGS) if isdir(join(LOGS, d))]
     for log_type in log_types:
-        runs = [d for d in os.listdir(log_type) if isdir(join(log_type, d))]
+        runs = [join(log_type, d) for d in os.listdir(log_type) if isdir(join(log_type, d))]
         for run in runs:
-            starts = [d for d in os.listdir(run) if isdir(join(run, d))]
+            starts = [join(run, d) for d in os.listdir(run) if isdir(join(run, d))]
             for start in starts:
-                path = join('logs', log_type, run, start)
-                archive_files = [f for f in os.listdir(path) if '.log.' in f]
+                archive_files = [f for f in os.listdir(start) if '.log.' in f]
                 if archive_files:
                     zip_file_name = (str(datetime.now())
                         .replace(' ', '_').replace('.', '_').replace(':', '_'))
-                    zip_file_path = join(path, zip_file_name + '.zip')
+                    zip_file_path = join(start, zip_file_name + '.zip')
                     zip_file = zipfile.ZipFile(
                         zip_file_path, mode='w',
                         compression=zipfile.ZIP_DEFLATED)
                     for f in archive_files:
-                        log_file = '{}/{}'.format(path, f)
-                        zip_file.write(log_file)
+                        log_file = '{}/{}'.format(start, f)
+                        zip_file.write(log_file, arcname=f)
                         os.remove(log_file)
+                    zip_file.close()
 
     logging.info('Archive end.')
-
-
-def dir_filter(x):
-    """Checks if x is a directory in the 'logs' folder.
-
-    Args:
-        x (str): The file name.
-
-    Returns:
-        bool: Whether the item is a directory.
-    """
-    return os.path.isdir('logs/{}'.format(x))
 
 
 def main():
